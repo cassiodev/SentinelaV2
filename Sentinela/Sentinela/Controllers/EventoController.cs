d62bf0f40c125ac792d2a921710b435d1676d490
cassiodev <cassio.iso@live.com>
2014-05-15 01:55:15 -0300

    acrécimos no evento

--- Sentinela/Sentinela/Controllers/EventoController.cs
+++ Sentinela/Sentinela/Controllers/EventoController.cs
@@ -22,6 +22,7 @@ namespace Sentinela.Controllers
         public ActionResult Index(int? ano, int? localId)
         {
             ViewBag.Local = _Contexto.Local.Where(l => l.Ativo).ToList();
+            ViewBag.Ano = ano ?? DateTime.Now.Year;
             return View();
         }
 
@@ -30,7 +31,7 @@ namespace Sentinela.Controllers
         public ActionResult GetEventos(int? ano, int? localId)
         {
 
-            var query = _Contexto.Evento.Where(e => e.Orcamento.DataEvento.Year == ano && (localId.HasValue && e.Orcamento.LocalId == localId.Value))
+            var query = _Contexto.Evento.Where(e => e.Data.Year == ano && (localId.HasValue && e.Orcamento.LocalId == localId.Value))
                                             .ToList()
                                             .Select(e => new
                                             {
@@ -61,10 +62,10 @@ namespace Sentinela.Controllers
         //
         // GET: /Evento/Create
 
-        public ActionResult Create(string data)
+        public ActionResult Create(string data, int? localId)
         {
 
-            return View(new Evento() {Data = Convert.ToDateTime(data) });
+            return View(new Evento() { Data = Convert.ToDateTime(data), LocalId = localId } );
         }
 
         //
@@ -74,28 +75,45 @@ namespace Sentinela.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Evento evento)
         {
-            if (evento.OrcamentoId != 0)
+            try
             {
-                //Valida orçamento
-                evento.Orcamento = _Contexto.Orcamento.Find(evento.OrcamentoId);
-                if (evento.Orcamento == null)
-                    return Json(new {erro = true, msg = "Orçamento não encontrado!" });
+                if (evento.OrcamentoId != 0 && evento.OrcamentoId != null)
+                {
+                    //Valida orçamento
+                    evento.Orcamento = _Contexto.Orcamento.Find(evento.OrcamentoId);
+                    if (evento.Orcamento == null)
+                        throw new Exception("Data não disponível");
+                }
+
+                //Valida data
+                if (_Contexto.Evento.Any(e => e.Data == evento.Data))
+                    throw new Exception("Data não disponível" );
+
+
+                if (ModelState.IsValid)
+                {
+                    _Contexto.Evento.Add(evento);
+                    _Contexto.SaveChanges();
+                    return Json(new { erro = false, msg = "Evento adicionado com sucesso!", ano = evento.Data.Year });
+                }
+
+                string msg = "Erro na validação de dados: ";
+
+                foreach (var item in ModelState.Values)
+                {
+                    foreach (var erros in item.Errors)
+                    {
+                        msg += erros.ErrorMessage;
+                    }
+                }
+
+                throw new Exception(msg);
             }
-
-            //Valida data
-            if (_Contexto.Evento.Any(e => string.Equals(e.Data.ToShortDateString(), evento.Data.ToShortDateString())))
-                return Json(new { erro = true, msg = "Data não disponível" });
-
-
-            if (ModelState.IsValid)
+            catch (Exception ex)
             {
-                _Contexto.Evento.Add(evento);
-                _Contexto.SaveChanges();
-                return RedirectToAction("Index");
+                return Json(new { erro = false, msg = ex.Message });
             }
-
-            ViewBag.OrcamentoId = new SelectList(_Contexto.Orcamento, "OrcamentoId", "Situacao", evento.OrcamentoId);
-            return View(evento);
+            
         }
 
         //
